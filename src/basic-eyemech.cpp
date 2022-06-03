ad3fe61e7bdf4481c0b6c845a4dcac7c8372970d
Brian Baker <bbaker47@gatech.edu>
2022-06-02 21:27:11 -0500

    Basic refactoring w/ defines
    - Use #defines for pins
    - Also #defines for servo indices
    - Remove unused "servonum"

--- src/basic-eyemech.cpp
+++ src/basic-eyemech.cpp
@@ -1,5 +1,5 @@
-//  Nilheim Mechatronics Simplified Eye Mechanism Code
-//  Make sure you have the Adafruit servo driver library installed >>>>> https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library
+//  (Modified) Nilheim Mechatronics Simplified Eye Mechanism Code
+
 //  X-axis joystick pin: A1
 //  Y-axis joystick pin: A0
 //  Trim potentiometer pin: A2
@@ -10,13 +10,22 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#define PIN_JOYSTICK_X A1
+#define PIN_JOYSTICK_Y A0
+#define PIN_EYELID_TRIM A2
+#define PIN_BLINK_BUTTON 2
+
+#define SERVO_EYE_X 0
+#define SERVO_EYE_Y 1
+#define SERVO_LID_UPPER_LEFT 2
+#define SERVO_LID_LOWER_LEFT 3
+#define SERVO_LID_UPPER_RIGHT 4
+#define SERVO_LID_LOWER_RIGHT 5
 
 #define SERVOMIN  140 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  520 // this is the 'maximum' pulse length count (out of 4096)
 
-// our servo # counter
-uint8_t servonum = 0;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 int xval;
 int yval;
@@ -34,7 +43,6 @@ int altlolidpulse;
 
 int trimval;
 
-const int analogInPin = A0;
 int sensorValue = 0;
 int outputValue = 0;
 int switchval = 0;
@@ -42,11 +50,10 @@ int switchval = 0;
 void setup() {
   Serial.begin(9600);
   Serial.println("8 channel Servo test!");
-  pinMode(analogInPin, INPUT);
+  pinMode(PIN_JOYSTICK_Y, INPUT);
   pinMode(2, INPUT);
 
   pwm.begin();
-
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
 
   delay(10);
@@ -70,51 +77,43 @@ void setServoPulse(uint8_t n, double pulse) {
 
 void loop() {
 
-  xval = analogRead(A1);
-    lexpulse = map(xval, 0,1023, 220, 440);
-    rexpulse = lexpulse;
+  xval = analogRead(PIN_JOYSTICK_X);
+  lexpulse = map(xval, 0, 1023, 220, 440);
+  rexpulse = lexpulse;
 
-    switchval = digitalRead(2);
+  switchval = digitalRead(PIN_BLINK_BUTTON);
 
+  yval = analogRead(PIN_JOYSTICK_Y);
+  leypulse = map(yval, 0, 1023, 250, 500);
+  reypulse = map(yval, 0, 1023, 400, 280);
 
-  yval = analogRead(A0);
-    leypulse = map(yval, 0,1023, 250, 500);
-    reypulse = map(yval, 0,1023, 400, 280);
+  trimval = analogRead(PIN_EYELID_TRIM);
+  trimval = map(trimval, 320, 580, -40, 40);
+  uplidpulse = map(yval, 0, 1023, 400, 280);
+  uplidpulse -= (trimval - 40);
+  uplidpulse = constrain(uplidpulse, 280, 400);
+  altuplidpulse = 680 - uplidpulse;
 
-  trimval = analogRead(A2);
-    trimval=map(trimval, 320, 580, -40, 40);
-     uplidpulse = map(yval, 0, 1023, 400, 280);
-        uplidpulse -= (trimval-40);
-          uplidpulse = constrain(uplidpulse, 280, 400);
-     altuplidpulse = 680-uplidpulse;
+  lolidpulse = map(yval, 0, 1023, 410, 280);
+  lolidpulse += (trimval / 2);
+  lolidpulse = constrain(lolidpulse, 280, 400);
+  altlolidpulse = 680 - lolidpulse;
 
-     lolidpulse = map(yval, 0, 1023, 410, 280);
-       lolidpulse += (trimval/2);
-         lolidpulse = constrain(lolidpulse, 280, 400);
-     altlolidpulse = 680-lolidpulse;
+  pwm.setPWM(SERVO_EYE_X, 0, lexpulse);
+  pwm.setPWM(SERVO_EYE_Y, 0, leypulse);
 
+  if (switchval == HIGH) {
+    pwm.setPWM(SERVO_LID_UPPER_LEFT, 0, 400);
+    pwm.setPWM(SERVO_LID_LOWER_LEFT, 0, 240);
+    pwm.setPWM(SERVO_LID_UPPER_RIGHT, 0, 240);
+    pwm.setPWM(SERVO_LID_LOWER_RIGHT, 0, 400);
+  } else if (switchval == LOW) {
+    pwm.setPWM(SERVO_LID_UPPER_LEFT, 0, uplidpulse);
+    pwm.setPWM(SERVO_LID_LOWER_LEFT, 0, lolidpulse);
+    pwm.setPWM(SERVO_LID_UPPER_RIGHT, 0, altuplidpulse);
+    pwm.setPWM(SERVO_LID_LOWER_RIGHT, 0, altlolidpulse);
+  }
 
-      pwm.setPWM(0, 0, lexpulse);
-      pwm.setPWM(1, 0, leypulse);
-
-
-      if (switchval == HIGH) {
-      pwm.setPWM(2, 0, 400);
-      pwm.setPWM(3, 0, 240);
-      pwm.setPWM(4, 0, 240);
-      pwm.setPWM(5, 0, 400);
-      }
-      else if (switchval == LOW) {
-      pwm.setPWM(2, 0, uplidpulse);
-      pwm.setPWM(3, 0, lolidpulse);
-      pwm.setPWM(4, 0, altuplidpulse);
-      pwm.setPWM(5, 0, altlolidpulse);
-      }
-
-
-
-          Serial.println(trimval);
-
+  Serial.println(trimval);
   delay(5);
-
 }
