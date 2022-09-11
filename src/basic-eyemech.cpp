4e18cb4d79326c99a12e5a0b33e4f05de9990198
Brian Baker <bbaker47@gatech.edu>
2022-09-11 12:14:18 -0500

    Remove unused setServoPulse

--- src/basic-eyemech.cpp
+++ src/basic-eyemech.cpp
@@ -48,23 +48,6 @@ void setup() {
   pwm.begin();
   pwm.setPWMFreq(50);  // Analog servos run at ~60 Hz updates // TODO: should be 50hz akshually
 	input.init();
-
-}
-
-// you can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= 60;   // 60 Hz
-  Serial.print(pulselength); Serial.println(" us per period");
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit");
-  pulse *= 1000000;  // convert to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-
 }
 
 void loop() {
